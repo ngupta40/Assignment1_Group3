b50cbbb78d97400ae4739f79e6d7a64ffcbcc8cc
Sharma <rsharm48@assu.eddu>
2017-09-30 16:37:10 -0700

    Poora code

--- app/src/main/java/com/example/nitesh/assignment1_group3/AccelService.java
+++ app/src/main/java/com/example/nitesh/assignment1_group3/AccelService.java
@@ -13,8 +13,11 @@ package com.example.nitesh.assignment1_group3;
         import android.hardware.SensorEvent;
         import android.hardware.SensorEventListener;
         import android.hardware.SensorManager;
+        import android.os.Binder;
         import android.os.Bundle;
+        import android.os.Handler;
         import android.os.IBinder;
+        import android.os.Message;
         import android.telephony.SmsManager;
         import android.widget.Toast;
 
@@ -22,9 +25,12 @@ public class AccelService extends Service implements SensorEventListener{
 
     private SensorManager accelManage;
     private Sensor senseAccel;
+    public Handler handler;
     float accelValuesX[] = new float[128];
     float accelValuesY[] = new float[128];
     float accelValuesZ[] = new float[128];
+    private String tableName = null;
+
     int index = 0;
     int k=0;
     Bundle b;
@@ -32,18 +38,16 @@ public class AccelService extends Service implements SensorEventListener{
     public void onSensorChanged(SensorEvent sensorEvent) {
         // TODO Auto-generated method stub
         Sensor mySensor = sensorEvent.sensor;
-
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            index++;
-            accelValuesX[index] = sensorEvent.values[0];
-            accelValuesY[index] = sensorEvent.values[1];
-            accelValuesZ[index] = sensorEvent.values[2];
-            if(index >= 127){
-                index = 0;
-                accelManage.unregisterListener(this);
-                //callFallRecognition();
-                accelManage.registerListener(this, senseAccel, SensorManager.SENSOR_DELAY_NORMAL);
-            }
+        System.out.println("Sending message outside if...."+handler);
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER && handler!=null) {
+            Bundle b = new Bundle();
+            Message msg = handler.obtainMessage();
+            b.putFloat("x", sensorEvent.values[0]);
+            b.putFloat("y", sensorEvent.values[1]);
+            b.putFloat("z", sensorEvent.values[2]);
+            msg.setData(b);
+            System.out.println("Sending message inside if....");
+            handler.sendMessage(msg);
         }
     }
 
@@ -211,22 +215,24 @@ public class AccelService extends Service implements SensorEventListener{
 
     @Override
     public void onCreate(){
+        System.out.println("\n\nOncreate of accelerometer....\n");
         //Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
         accelManage = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senseAccel = accelManage.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         accelManage.registerListener(this, senseAccel, SensorManager.SENSOR_DELAY_NORMAL);
-        System.out.println("\n\nOncreate of accelerometer....\n");
+
 
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("\n\nOn START of accelerometer....\n");
         b = intent.getExtras();
-        String phoneNumber = b.getString("tableName");
-        Toast.makeText(AccelService.this, phoneNumber, Toast.LENGTH_LONG).show();
+        String newTableName = b.getString("tableName");
+        Toast.makeText(AccelService.this, newTableName, Toast.LENGTH_LONG).show();
         // We want this service to continue running until it is explicitly
         // stopped, so return sticky.
         //k = 0;*/
-        System.out.println("\n\nOn START of accelerometer....\n");
+        this.tableName = newTableName;
         return START_STICKY;
     }
 
@@ -235,6 +241,18 @@ public class AccelService extends Service implements SensorEventListener{
         // TODO Auto-generated method stub
 
         return null;
+    }
+
+    public class LocalBinder extends Binder {
+        public AccelService getInstance(){
+            //System.out.println("\n\n\nReturning accelerometer service\n\n\n");
+            return AccelService.this;
+        }
+    }
+
+    public void setHandler(Handler handler){
+        System.out.println("\n\nSetting handler"+handler+"\n\n");
+        this.handler = handler;
     }
 
 }
