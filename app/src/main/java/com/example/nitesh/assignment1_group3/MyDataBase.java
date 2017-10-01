3d2773d88c29629126d945edc14313b2a03e3788
ngupta40 <ngupta40@asu.edu>
2017-10-01 16:24:04 -0700

    Changes  - Karan

--- app/src/main/java/com/example/nitesh/assignment1_group3/MyDataBase.java
+++ app/src/main/java/com/example/nitesh/assignment1_group3/MyDataBase.java
@@ -9,6 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import android.os.Environment;
 
 /**
  * Created by Nitesh Gupta on 01-10-2017.
@@ -16,10 +17,10 @@ import java.util.ArrayList;
 
 public class MyDataBase extends SQLiteOpenHelper
 {
-
-    private static final String DATABASE_NAME = "Group2_pg_avr.db";
+    private static String path = Environment.getExternalStorageDirectory().getPath();
+    private static String DATABASE_FILENAME = "/group3";
+    private static final String DATABASE_NAME = path+ "/Android/Data/CSE535_ASSIGNMENT2" + DATABASE_FILENAME;
     private static final int DATABASE_VERSION = 1;
-    private final String DATABASE_PATH;
     private Context context;
 
     @Override
@@ -30,21 +31,18 @@ public class MyDataBase extends SQLiteOpenHelper
     public MyDataBase(Context context)
     {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        DATABASE_PATH = context.getFilesDir().getPath()+"/";
         this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        Log.w(this.getClass().getSimpleName(), " OnCreate");
     }
 
     public void createDatabase() {
         SQLiteDatabase sqLiteDatabase = null;
-        String database = DATABASE_PATH + DATABASE_NAME;
+        String database = DATABASE_NAME;
         try {
             sqLiteDatabase = SQLiteDatabase.openOrCreateDatabase(database, null);
-            Log.w(this.getClass().getSimpleName(), "Database exists");
         } catch (Exception e) {
             Log.w(this.getClass().getSimpleName(), "Database doesn't exist");
         }
@@ -52,8 +50,7 @@ public class MyDataBase extends SQLiteOpenHelper
 
     public void createTable(String name) {
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + name + "(timestamp datetime, xvalue real, yvalue real, zvalue real);");
-        Log.w("Table: ", "Creation successful");
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + name + "(timestamp datetime, xValue real, yValue real, zValue real);");
         db.close();
     }
 
@@ -62,41 +59,37 @@ public class MyDataBase extends SQLiteOpenHelper
     public ArrayList<float[]> fetchLastTenValues(String name){
         try{
             SQLiteDatabase db = getWritableDatabase();
-            Cursor cursor =  db.rawQuery("SELECT xvalue, yvalue , zvalue FROM " + name + " ORDER BY TIMESTAMP DESC LIMIT 10;", null);
-            float[] xvalues = new float[10];
-            float[] yvalues = new float[10];
-            float[] zvalues = new float[10];
+            Cursor cursor =  db.rawQuery("SELECT xValue, yValue , zValue FROM " + name + " ORDER BY timestamp DESC LIMIT 10;", null);
+            float[] xValues = new float[10];
+            float[] yValues = new float[10];
+            float[] zValues = new float[10];
 
             cursor.moveToFirst();
             for(int i=9;i>=0;i--) {
                 if(!cursor.isAfterLast()){
-                    float xvalue = cursor.getFloat(cursor.getColumnIndex("xvalue"));
-                    xvalues[i] = (float) xvalue;
+                    float xVal = cursor.getFloat(cursor.getColumnIndex("xValue"));
+                    xValues[i] = (float) xVal;
 
-                    float yvalue = cursor.getFloat(cursor.getColumnIndex("yvalue"));
-                    yvalues[i] = (float) yvalue;
+                    float yVal = cursor.getFloat(cursor.getColumnIndex("yValue"));
+                    yValues[i] = (float) yVal;
 
-                    float zvalue = cursor.getFloat(cursor.getColumnIndex("zvalue"));
-                    zvalues[i] = (float) zvalue;
+                    float zVal = cursor.getFloat(cursor.getColumnIndex("zValue"));
+                    zValues[i] = (float) zVal;
                     cursor.moveToNext();
-                    Log.w("here: ", "testing");
                 } else {
-                    xvalues[i] = 0;
-                    yvalues[i]=0;
-                    zvalues[i]=0;
+                    xValues[i] = 0;
+                    yValues[i]=0;
+                    zValues[i]=0;
                 }
             }
-            Log.w("Retrieved: ", "successful");
 
-            ArrayList<float[]> accelArray= new ArrayList<float[]>();
-            accelArray.add(xvalues);
-            accelArray.add(yvalues);
-            accelArray.add(zvalues);
-            System.out.println(accelArray);
-            return accelArray;
+            ArrayList<float[]> accelArr = new ArrayList<float[]>();
+            accelArr.add(xValues);
+            accelArr.add(yValues);
+            accelArr.add(zValues);
+            return accelArr;
         }
         catch(SQLiteException ex){
-            Log.w("Query Exception", "Unable to fetch last 10 records from table "+ name + ";");
             ex.printStackTrace();
         }
         return null;
@@ -105,23 +98,20 @@ public class MyDataBase extends SQLiteOpenHelper
     public void insertAccelValues(String name, Timestamp timestamp, float x, float y, float z){
         try{
             SQLiteDatabase db = getWritableDatabase();
-            Log.w("Insert statement -> ", "Timestamp -> " + timestamp.toString() + " xvalue -> " + x + " yvalue -> " + y + "zvalue -> " + z );
             try{
                 ContentValues values = new ContentValues();
                 values.put("timestamp", System.currentTimeMillis());
-                values.put("xvalue", x);
-                values.put("yvalue", y);
-                values.put("zvalue", z);
+                values.put("xValue", x);
+                values.put("yValue", y);
+                values.put("zValue", z);
                 db.insertOrThrow(name, null, values);
 
                 Cursor cursor = db.rawQuery("SELECT * FROM " + name, null);
                 if(cursor.getCount() > 1){
                     Log.w("None Exist in db", "Nothing inserted");
                 }
-                Log.w("Added: ", "successful");
             }
             catch (SQLiteException ex){
-                Log.w("Insert Exception", "Unable to insert to table "+ name + " ->  timestamp: " + timestamp + ", x: "+ x + ", y: "+ y+ ", z: "+ z);
                 ex.printStackTrace();
             }
             finally {
@@ -133,4 +123,3 @@ public class MyDataBase extends SQLiteOpenHelper
         }
     }
 }
-
